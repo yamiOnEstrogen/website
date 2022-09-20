46927d6f36d9f2963196ae137209697e95ae0742
akenolol <zzerzv_dev@outlook.com>
2022-09-20 10:55:40 -0400

    Update script.js
    Signed-off-by: akenolol <zzerzv_dev@outlook.com>

--- public/script.js
+++ public/script.js
@@ -1,7 +1,25 @@
-const vats = ["DE", "FR", "UK", "US"];
-const vat = vats[Math.floor(Math.random() * vats.length)];
-console.log(vat);
+const websiteConfig = [{
+    name: 'AkenoDev Website',
+    url: 'https://akenodev.xyz',
+    description: 'AkenoDev is a website that is used to host my projects and other things.',
+    acceptHeaders: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Cache-Control': 'max-age=0',
+        'Connection': 'keep-alive',
+        'Host': null,
+        'Upgrade-Insecure-Requests': '1',
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'none',
+        'Sec-Fetch-User': '?1',
+        'Sec-GPC': '1',
+    },
+}];
 
 
-// Language: javascript
-// Path: public/script.js
+console.log('------------------------------------------------------------------------------------------------------');
+console.log(websiteConfig);
+console.log('------------------------------------------------------------------------------------------------------');
