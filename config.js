eb0e13b39ce6fb666ced49a37579c1dd2ef60c78
Akeno <zzerzv_dev@outlook.com>
2022-09-29 17:16:52 -0400

    Create config.js
    Signed-off-by: Akeno <zzerzv_dev@outlook.com>

---
+++ config.js
@@ -0,0 +1,39 @@
+module.exports.webApp = {
+    port: 3000,
+    host: "http://localhost",
+},
+
+module.exports.owner = {
+    id: "YOUR-DISCORD-ID",
+    botToken: "YOUR-DISCORD-BOT-TOKEN",
+    githubToken: "YOUR-GITHUB-TOKEN"
+},
+
+
+module.exports.projects = {
+    1: {
+        name: "akenolol.github.io",
+        description: "The Official Github Repo for akenodev.xyz.",
+        url: "https://github.com/akenolol/akenolol.github.io",
+        image: "https://cdn.discordapp.com/icons/1014190469628055552/a_a92a1b63d4ae364ea0e17ad6526bd16b.gif",
+    },
+    2: {
+        name: "Goddess-Bot",
+        description: "The Source Code for The Server Goddess (https://cafe.akenodev.xyz.invite/).",
+        url: "https://github.com/akenolol/Goddess-Bot",
+        image: "https://cdn.discordapp.com/avatars/1006316455886860329/8df894fb0eb61a03200d86d5eaf52867.png?size=1024",
+    },
+    3: {
+        name: "react-todo-app",
+        description: "A simple todo app made with React.",
+        url: "https://github.com/akenolol/react-todo-app",
+        image: "https://camo.githubusercontent.com/478db36abc8409d077227c7e2ca39729de00c6f42a124a8324c2df04450289d6/68747470733a2f2f6170692e6672616d65722e636f6d2f73746f72652f6173736574732f69736d61656c2f626f6f7473747261702d69636f6e732f617274776f726b2e706e673f4f44686d4e4449"
+    },
+    4: {
+        name: "Basic-Web-Server",
+        description: "A Basic Web Server in nodejs. Built with express.",
+        url: "https://github.com/akenolol/Basic-Web-Server",
+        image: "https://innovationyourself.com/wp-content/uploads/2020/08/nodejs-logo.png"
+    }
+
+}
