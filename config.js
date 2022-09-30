64acdd80ef73ffe4cfe2ed20c1b33601b8de6d53
Akeno <zzerzv_dev@outlook.com>
2022-09-29 20:43:02 -0400

    Update config.js
    Signed-off-by: Akeno <zzerzv_dev@outlook.com>

--- config.js
+++ config.js
@@ -10,30 +10,92 @@ module.exports.owner = {
 },
 
 
+module.exports.languages = {
+    "JavaScript": "JavaScript",
+    "TypeScript": "TypeScript",
+    "HTML": "HTML",
+    "CSS": "CSS",
+    "Python": "Python",
+    "Java": "Java",
+    "C#": "C#",
+    "C++": "C++",
+    "C": "C",
+    "PHP": "PHP",
+    "Ruby": "Ruby",
+    "Go": "Go",
+    "Rust": "Rust",
+    "Swift": "Swift",
+    "Kotlin": "Kotlin",
+    "Dart": "Dart",
+    "R": "R",
+    "Shell": "Shell",
+    "Objective-C": "Objective-C",
+};
+
+module.exports.frameworks = {
+    "React": "React",
+    "Vue": "Vue",
+    "Angular": "Angular",
+    "Express": "Express",
+    "Django": "Django",
+    "Flask": "Flask",
+    "Laravel": "Laravel",
+    "Spring": "Spring",
+    "Discordjs": "Discord.js",
+    "Discordpy": "Discord.py",
+    "Bodyparser": "body-parser",
+    "Cors": "Cors",
+    "Ejs": "Ejs",
+}
+
 module.exports.projects = {
     1: {
-        name: "akenolol.github.io",
+        name: "akenodev.xyz",
         description: "The Official Github Repo for akenodev.xyz.",
-        url: "https://github.com/akenolol/akenolol.github.io",
+        url: "https://github.com/akenolol/akenodev.xyz",
         image: "https://cdn.discordapp.com/icons/1014190469628055552/a_a92a1b63d4ae364ea0e17ad6526bd16b.gif",
+        language: `${this.languages.JavaScript} | ${this.languages.HTML} | ${this.languages.CSS}`,
+        framework: `${this.frameworks.Express} | ${this.frameworks.Bodyparser} | ${this.frameworks.Cors} | ${this.frameworks.Ejs}`,
     },
     2: {
         name: "Goddess-Bot",
         description: "The Source Code for The Server Goddess (https://cafe.akenodev.xyz.invite/).",
         url: "https://github.com/akenolol/Goddess-Bot",
         image: "https://cdn.discordapp.com/avatars/1006316455886860329/8df894fb0eb61a03200d86d5eaf52867.png?size=1024",
+        language: `${this.languages.JavaScript}`,
+        framework: `${this.frameworks.Discordjs} | ${this.frameworks.Express}`,
     },
     3: {
         name: "react-todo-app",
         description: "A simple todo app made with React.",
         url: "https://github.com/akenolol/react-todo-app",
-        image: "https://camo.githubusercontent.com/478db36abc8409d077227c7e2ca39729de00c6f42a124a8324c2df04450289d6/68747470733a2f2f6170692e6672616d65722e636f6d2f73746f72652f6173736574732f69736d61656c2f626f6f7473747261702d69636f6e732f617274776f726b2e706e673f4f44686d4e4449"
+        image: "https://cdn.discordapp.com/attachments/981632133191843852/1025205816472653884/3203-reactjs.png",
+        language: `${this.languages.JavaScript} | ${this.languages.CSS} | ${this.languages.HTML}`,
+        framework: `${this.frameworks.React}`,
     },
     4: {
         name: "Basic-Web-Server",
         description: "A Basic Web Server in nodejs. Built with express.",
         url: "https://github.com/akenolol/Basic-Web-Server",
-        image: "https://innovationyourself.com/wp-content/uploads/2020/08/nodejs-logo.png"
+        image: "https://cdn.discordapp.com/attachments/981632133191843852/1025205418194120745/4408_nodejs.png",
+        language: `${this.languages.JavaScript}`,
+        framework: `${this.frameworks.Express} | ${this.frameworks.Bodyparser} | ${this.frameworks.Cors} `,
+    },
+    5: {
+        name: "c-program",
+        description: "A Simple Program to Show How C Works.",
+        url: "https://github.com/akenolol/c-program",
+        image: "https://cdn.iconscout.com/icon/free/png-512/c-programming-569564.png",
+        language: `${this.languages.C}`,
+        framework: "None",
+    },
+    6: {
+        name: "Akeno-Bartender-Bot",
+        description: "The Discord bot For https://discord.gg/g78PTzfMMP",
+        url: "https://github.com/akenolol/Akeno-Bartender-Bot",
+        image: "https://cdn.discordapp.com/attachments/981632133191843852/1025204888944267306/DJNekoWag.gif",
+        language: `${this.languages.JavaScript}`,
+        framework: `${this.frameworks.Discordjs}`,
     }
 
 }
