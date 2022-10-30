518c6e2dd6b24afec8a3eb07f20f73d1d3472182
Hayden <zzerzv_dev@outlook.com>
2022-10-30 19:50:54 -0400

    Add files via upload
    Signed-off-by: Hayden <zzerzv_dev@outlook.com>

---
+++ config.js
@@ -0,0 +1,263 @@
+module.exports.webApp = {
+    port: 8080,
+    isOffline: false,
+},
+
+    module.exports.owner = {
+        id: "547923574833545226",
+        supportServer: "https://discord.gg/d9fd8CHWP3",
+    },
+
+    module.exports.items = {
+        "8564564": {
+            "name": "Discord Bot",
+            "id": "8564564",
+            "description": "A discord bot that can be used for moderation, fun, and more.",
+            "price": 10,
+          },
+          "5473524": {
+            "name": "Website",
+            "id": "5473524",
+            "description": "A website that can be used for anything.",
+            "price": 20,
+          },
+          "4289640": {
+            "name": "Twitter Bot",
+            "id": "4289640",
+            "description": "A twitter bot that can be used for moderation, fun, and more.",
+            "price": 15,
+          },
+          "78403650": {
+            "name": "Minecraft Plugin",
+            "id": "78403650",
+            "description": "A Minecraft Plugin that can be used for PvP, Minigames, and more.",
+            "price": 10,
+          },
+    }
+
+
+
+    module.exports.news = {
+        1: {
+    title: "akenodev.xyz goes Open Source!",
+    description: `akeno, has now made akenodev.xyz open source on github!`,
+    date: "2022-9-5",
+    image: "./assets/img/news/akenoDevGithubBanner.png",
+    link: "https://github.com/akenolol/akenodev.xyz",
+  },
+  2: {
+    title: "Discord Integration Comes to akenodev.xyz!",
+    description: `akeno, has now added the option to Login with Discord, to purchase items.`,
+    date: "2022-10-17",
+    image: "./assets/img/news/discordLogoBanner.png",
+    link: "https://discord.com",
+  },
+         3: {
+            title: "Aqua the Goddess Comes to Red Cafe!",
+            description: `Aqua the Water Goddess comes to Red Cafe!`,
+            date: "2022-10-23",
+            image: "./assets/img/news/aquaGoddess.png",
+            link: "https://akenodev.xyz/invite"
+        },
+    },
+
+    module.exports.languages = {
+        "JavaScript": {
+            name: "JavaScript",
+            options: {
+                percentile: "98"
+            }
+        },
+        "TypeScript": {
+            name: "TypeScript",
+            options: {
+                percentile: "90"
+            }
+        },
+        "HTML": {
+            name: "HTML",
+            options: {
+                percentile: "69"
+            }
+        },
+        "C#": {
+            name: "C#",
+            options: {
+                percentile: "90"
+            }
+        },
+        "C": {
+            name: "C",
+            options: {
+                percentile: "50"
+            }
+        },
+    };
+
+module.exports.frameworks = {
+    React: {
+        name: "React",
+        options: {
+            percentile: "44",
+        },
+    },
+    Vue: {
+        name: "Vue",
+        options: {
+            percentile: "32",
+        },
+    },
+    Angular: {
+        name: "Angular",
+        options: {
+            percentile: "27",
+        },
+    },
+    EJS: {
+        name: "EJS",
+        options: {
+            percentile: "98",
+        }
+    },
+    Electron: {
+        name: "Electron",
+        options: {
+            percentile: "43",
+        }
+    }
+};
+
+
+module.exports.projects = {
+  1: {
+    name: "akenodev.xyz",
+    image: "/assets/img/projects/akenoWebsite.gif",
+    role: "websites",
+    button: {
+      name: "View Repository",
+      url: "https://github.com/akenolol/akenodev.xyz",
+      color: "primary",
+    },
+  },
+  2: {
+    name: "React todo app",
+    image: "/assets/img/projects/reactTodoApp.png",
+    role: "websites tools",
+    button: {
+      name: "View Repository",
+      url: "https://github.com/akenolol/react-todo-app",
+      color: "primary",
+    },
+  },
+  3: {
+    name: "Goddess Bot",
+    image: "/assets/img/projects/goddessBot.png",
+    role: "bots tools",
+    button: {
+      name: "View Repository",
+      url: "https://github.com/akenolol/Goddess-Bot",
+      color: "primary",
+    },
+  },
+  4: {
+    name: "Epicgames npm",
+    image: "/assets/img/projects/epicGames.png",
+    role: "packages tools",
+    button: {
+      name: "Install Package",
+      url: "https://www.npmjs.com/package/@akenodev/epicgames",
+      color: "danger",
+      warning: "When you install this package, *PLEASE* credit me. Thank you.",
+    },
+  },
+  5: {
+    name: "Discord Info",
+    image: "/assets/img/projects/info.png",
+    role: "tools",
+    button: {
+      name: "View Repository",
+      url: "https://github.com/akenolol/Discord-Info",
+      color: "primary",
+    },
+  },
+  6: {
+    name: "Devo Bot",
+    image: "/assets/img/projects/devoBot.png",
+    role: "bots tools",
+    button: {
+      name: "View Repository",
+      url: "https://github.com/akenolol/Devo-Bot",
+      color: "primary",
+    },
+  },
+  7: {
+    name: "Discord Webhook C#",
+    image: "/assets/img/projects/discordLogo.png",
+    role: "tools",
+    button: {
+      name: "View Repository",
+      url: "https://github.com/akenolol/Discord-Webhook-CSharp",
+      color: "primary",
+    },
+  },
+  8: {
+    name: "Verbose Bot",
+    image: "/assets/img/projects/verboseBot.png",
+    role: "tools bots",
+    button: {
+      name: "View Repository",
+      url: "https://github.com/akenolol/verbose-bot",
+      color: "primary",
+    },
+  },
+};
+
+module.exports.profile = {
+    aboutMe: ["Voice Actor", "Web Developer", "Discord / Telegram Bot Developer", "Discord Moderator", "Reddit Moderator", "Software Developer", "Anime Voice Actor"],
+    socials: {
+        github: "https://github.com/akenolol",
+        twitter: "https://twitter.com/akeno_dev",
+        reddit: "https://www.reddit.com/user/premage1010",
+        discord: "https://discordapp.com/users/547923574833545226",
+        telegram: "https://t.me/akenodev",
+    },
+    languages: [
+        " en",
+        " jp",
+    ],
+
+
+
+}
+
+module.exports.clients = {
+    1: {
+        name: "Remi",
+        image: "/assets/img/clients/remi.png",
+        discriminator: "7095",
+        rating: "5",
+        discription: "Ordered a custom bot from akeno, and it was amazing! He was very helpful and was able to make the bot exactly how I wanted it to be. I would highly recommend him to anyone who is looking for a custom bot!",
+        whois: {
+            name: "Discord Server Member",
+            url: "https://akenodev.xyz/invite",
+        },
+        isInServer: true,
+        didContributeToRepo: false,
+
+    },
+    2: {
+        name: "LunarDev",
+        image: "/assets/img/clients/default.png",
+        discriminator: "1265",
+        rating: "5",
+        discription: "I have worked with akeno on a few projects, and he is a great developer. He is very helpful and is always willing to help. I would highly recommend him to anyone who is looking for a developer.",
+        whois: {
+            name: "Github Contributor",
+            url: "https://akenodev.xyz/invite",
+        },
+        isInServer: false,
+        didContributeToRepo: true,
+
+    }
+};
+
---
+++ index.js
@@ -0,0 +1,345 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const path = require("path");
+const fs = require("fs");
+const Logger = require("./utils/logger.js");
+const logger = new Logger({ debug: true });
+const os = require("os");
+const DiscordClient = require("./utils/discordClient.js");
+const config = require("./config");
+const port = process.env.PORT || config.webApp.port;
+const { Octokit } = require("@octokit/core");
+const { URLSearchParams } = require('url');
+const fetch = require("node-fetch");
+const axios = require("axios");
+const dotenv = require("dotenv");
+dotenv.config();
+const discordClient = new DiscordClient(process.env.token);
+
+
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.engine(".ejs", ejs.__express);
+app.use(express.static("public"));
+app.use((req, res, next) => {
+  if (config.webApp.isOffline) {
+    res.render("down.ejs", {
+    });
+  } else {
+    next();
+  }
+})
+
+
+
+
+app.get("/", async (req, res) => {
+  discordClient.getOwnerInfo().then(async (owner) => {
+
+
+    const codingLanguagesArray = [];
+    const clientUsers = [];
+    const frameworksArray = [];
+    const projectsArray = [];
+    const newsArray = [];
+    const serverWidget = await discordClient.getServerWidget(`1014190469628055552`, "shield");
+
+    const socials = config.profile.socials;
+    const codingLanguages = config.languages;
+    const frameworks = config.frameworks;
+    const projects = config.projects;
+
+
+    for (const project in projects) {
+      projectsArray.push(projects[project]);
+    }
+
+
+
+    for (const language in codingLanguages) {
+      codingLanguagesArray.push(codingLanguages[language]);
+    }
+
+    for (const framework in frameworks) {
+      frameworksArray.push(frameworks[framework]);
+    }
+
+    for (const news in config.news) {
+      newsArray.push(config.news[news]);
+    }
+    
+    
+    for (const client in config.clients) {
+      clientUsers.push(config.clients[client]);
+    }
+  
+
+    res.render("index", {
+      owner: owner,
+      supportServer: config.owner.supportServer,
+      codingLanguages: codingLanguagesArray,
+      aboutMe: config.profile.aboutMe,
+      languages: config.profile.languages,
+      socials: socials,
+      frameworks: frameworksArray,
+      serverWidget: serverWidget,
+      news: newsArray,
+      projects: projectsArray,
+      clients: clientUsers,
+
+    });
+  })
+});
+
+
+app.get("/login", (req, res) => {
+  res.redirect("https://discord.com/api/oauth2/authorize?client_id=1025115275114319883&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Foauth2&response_type=code&scope=identify%20email%20guilds.join")
+});
+
+app.get("/spotify", async (req, res) => {
+  res.redirect("/redirect?url=https://open.spotify.com/playlist/3bZ1UCRHaChgW1pIsdvxnw?si=03a44d18212848d1&title=Spotify%20Playlist");
+})
+
+app.get("/api/:field", async (req, res) => {
+  const field = req.params.field;
+
+
+    if (field === "news") {
+      res.json(config.news);
+    }
+    if (field === "projects") {
+      res.json(config.projects);
+    }
+    if (field === "items") {
+      res.json(config.items);
+    }
+    if (field === "clients") {
+    res.json(config.clients);
+  }
+
+})
+
+app.get("/oauth2", async (req, res) => {
+  const data_1 = new URLSearchParams();
+  data_1.append('client_id', process.env.clientId);
+  data_1.append('client_secret', process.env.clientSecret);
+  data_1.append('grant_type', 'authorization_code');
+  data_1.append('redirect_uri', `http://localhost:8080/oauth2`);
+  data_1.append('scope', 'identify email');
+  data_1.append('code', req.query.code);
+
+  fetch('https://discord.com/api/oauth2/token', { method: "POST", body: data_1 }).then(response => response.json()).then(data2 => {
+    const options = {
+      method: 'GET',
+      url: 'https://discord.com/api/users/@me',
+      headers: {
+        'Authorization': `Bearer ${data2.access_token}`
+      }
+    }
+
+
+    axios.get(options.url, { headers: options.headers }).then(response => {
+
+
+      const data = response.data;
+
+      console.log(data)
+
+      discordClient.joinSupportServer(data2.access_token, data.id).then(() => {
+        console.log("Joined support server")
+        res.render("purchase", {
+          username: data.username,
+          discriminator: data.discriminator,
+          id: data.id,
+          avatar: data.avatar,
+          email: data.email,
+        });
+      })
+
+
+
+    }).catch(err => {
+      console.log(err);
+    })
+
+  })
+
+});
+
+app.get("/cv", async (req, res) => {
+  res.redirect("https://docs.google.com/document/d/1tBfrkcgccE9dBIJFmCLS3BvLUhQcinSa0wAaE_0cUn4/edit?usp=sharing")
+})
+
+app.post("/purchase", async (req, res) => {
+  const item = req.body.item;
+  const user = req.body.user;
+  //  res.send("Purchase has been sent to the owner!, He will contact you soon! (EMAIL)");
+
+  discordClient.postPurchaseMessage({
+    item: item,
+    user: user,
+  }).then(() => {
+    res.send("Purchase has been sent to the owner!, He will contact you soon! (EMAIL)");
+  })
+})
+
+
+app.get("/error", (req, res) => {
+  const error = req.query.err;
+  const code = req.query.code;
+  if (error == null || error == "" || error == undefined) {
+    res.json({
+      error: "No error provided"
+    });
+  }
+  else {
+    res.render("error.ejs", {
+      error: error,
+      code: code,
+    })
+  }
+})
+
+app.post("/contact", (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const subject = req.body.subject;
+  const message = req.body.text;
+
+  discordClient.postContactMessage(name, email, subject, message).then((channel) => {
+    res.json({
+      success: "Message sent successfully",
+      message: channel,
+    })
+  })  
+})
+
+app.post("/bug", (req, res) => {
+  const name = req.body.name;
+  const expectedResult = req.body.expectedResult;
+  const actualResult = req.body.actualResult;
+  const howToReproduce = req.body.howToReproduce;
+
+  discordClient.postBugMessage(name, expectedResult, actualResult, howToReproduce).then((channel) => {
+    res.json({
+      success: "Bug sent successfully",
+      message: "If you are in the support server, please DM akeno#1010, and we will get back to you as soon as possible.",
+    })
+  })
+})
+
+app.get("/reportbug", async (req, res) => {
+  res.render("reportBug.ejs", {
+
+  });
+})
+
+app.get("/redirect", (req, res) => {
+  const url = req.query.url;
+  const title = req.query.title || "akenodev redirect feature";
+
+  if (url == null || url == "" || url == undefined) {
+    res.json({
+      error: "No url provided"
+    });
+  }
+  else {
+    res.render("redirect", {
+      REDIRECT_URL: url,
+      REDIRECT_TITLE: title,
+      HOST: req.hostname,
+      REDIRECT_URL_SHORT: url
+        .replace("https://", "")
+        .replace("http://", "")
+        .replace("www.", "")
+        .split("/")[0],
+      baseUrl: req.protocol + "://" + req.get("host"),
+    });
+    logger.log("Redirecting to " + url);
+  }
+});
+
+
+app.get("/status", (req, res) => {
+  status.getStatus().then((status) => {
+    res.render("status", {
+      homePage: status.homePage,
+      redirectPage: status.redirectPage,
+    });
+  })
+})
+
+
+app.get("/invite", (req, res) => {
+  res.redirect(`/redirect?url=${config.owner.supportServer}`)
+});
+
+app.get("/items", (req, res) => {
+
+  const itemLists = {
+    "8564564": {
+      "name": "Discord Bot",
+      "id": "8564564",
+      "description": "A discord bot that can be used for moderation, fun, and more.",
+      "price": 10,
+    },
+    "5473524": {
+      "name": "Website",
+      "id": "5473524",
+      "description": "A website that can be used for anything.",
+      "price": 20,
+    },
+    "4289640": {
+      "name": "Twitter Bot",
+      "id": "4289640",
+      "description": "A twitter bot that can be used for moderation, fun, and more.",
+      "price": 15,
+    },
+    "78403650": {
+      "name": "Minecraft Plugin",
+      "id": "78403650",
+      "description": "A minecraft plugin that can be used for moderation, fun, and more.",
+      "price": 10,
+    },
+
+  }
+
+  res.json(itemLists);
+
+})
+
+
+
+app.listen(port, () => {
+  console.clear()
+
+  const interfaces = os.networkInterfaces();
+  const addresses = [];
+  for (k in interfaces) {
+    for (k2 in interfaces[k]) {
+      const address = interfaces[k][k2];
+      if (address.family === 'IPv4' && !address.internal) {
+        addresses.push(address.address);
+      }
+    }
+  }
+
+
+
+  logger.log(`To View on your Machine go to: http://localhost:${port}`);
+  logger.log(`To View on your Network go to: http://${addresses[0]}:${port}`);
+
+});
+
+
+
+
+
+
+
+
