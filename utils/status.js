e4bb11d4b748f65ec042d12164ba60fc40e169e4
Stella <predevxyzlol@gmail.com>
2023-02-28 18:31:18 -0500

    Create status.js
    Signed-off-by: Stella <predevxyzlol@gmail.com>

---
+++ utils/status.js
@@ -0,0 +1,69 @@
+const axios = require("axios");
+
+
+const urls = [
+    {
+        url: "https://home.unfatal.xyz",
+        name: "Unfatal",
+    },
+    {
+        url: "https://1.kiyodev.xyz",
+        name: "kiyodev.xyz",
+    }
+];
+
+const listOfCodes = [
+    {
+        code: 200,
+        name: "Operational",
+    },
+    {
+        code: 400,
+        name: "Bad Request",
+    },
+    {
+        code: 404,
+        name: "Not Found",
+    },
+    {
+        code: 408,
+        name: "Request Timeout",
+    },
+    {
+        code: 500,
+        name: "Internal Server Error",
+    },
+    {
+        code: 502,
+        name: "Bad Gateway",
+    },
+    {
+        code: 503,
+        name: "Service Unavailable",
+    }
+]
+
+
+class Status {
+    constructor() {}
+
+
+    async getStatusCodes() {
+        const statusCodes = [];
+        for (const url of urls) {
+            const response = await axios.get(url.url);
+            statusCodes.push({
+                name: url.name,
+                link: url.url,
+                code: {
+                    code: response.status,
+                    name: listOfCodes.find((code) => code.code === response.status).name,
+                    isBad: response.status >= 400 ? true : false,
+                }
+            });
+        }
+        return statusCodes;
+    }
+}
+
+module.exports = new Status();
