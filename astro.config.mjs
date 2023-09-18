9d190cc3d7f65897b04dfeea2a86f8570a0688c6
ステラ <live.hylia@outlook.com>
2023-09-18 09:22:33 -0400

    Compress The HTML

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
 import { defineConfig } from 'astro/config';
+import Compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
     server: {
         port: 4321
-    }
+    },
+
+    integrations: [Compress({
+        CSS: true,
+        HTML: true,
+        Image: false,
+        JavaScript: true,
+        SVG: false,
+    }),]
 });
