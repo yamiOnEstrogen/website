526adb2a72cb4a9afefcbf3730edb71e2f5aa13e
ステラ <live.hylia@outlook.com>
2023-10-21 11:39:46 -0400

    New Version (test)

--- src/utils/CreateShareLinks.ts
+++ src/utils/CreateShareLinks.ts
@@ -5,7 +5,7 @@
 export default function CreateShareLinks(title: string, url: string) {
     return `<li class="list-inline-item">
           <a
-              href="https://twitter.com/intent/tweet?text=Check out ${title} on @char32vt's blog.%0A%0A${url}"
+              href="https://twitter.com/intent/tweet?text=Check out ${title} on @withervt's blog.%0A%0A${url}"
               target="_blank"
               rel="noopener noreferrer nofollow"
           >
